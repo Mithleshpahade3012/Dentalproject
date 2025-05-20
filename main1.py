ee2f2ece590cc0ae72e712c9c3d87a753d8de04c
Mithleshpahade3012 <mithleshpahade3012@gmail.com>
2025-05-20 13:07:36 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -208,4 +208,4 @@ asgi_app = WsgiToAsgi(app)
 if __name__ == "__main__":
     import uvicorn  
 
-    uvicorn.run(asgi_app, host="127.0.0.1", port=8000)
+    uvicorn.run(asgi_app, host="0.0.0.0", port=8000)
