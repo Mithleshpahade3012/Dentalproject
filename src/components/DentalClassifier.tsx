c794468be2334134b4942cd13a641b1cbf5bf3ae
Mithlesh Pahade <MPahade@bpl.netlink.com>
2025-05-23 23:56:28 +0530

    added just domain name

--- src/components/DentalClassifier.tsx
+++ src/components/DentalClassifier.tsx
@@ -29,7 +29,7 @@ export default function DentalClassifier() {
     formData.append("file", file);
 
     try {
-      const response = await fetch("https://127.0.0.1:8000/predict/", {
+      const response = await fetch("https://heartofindia.co/api/predict/", {
         method: "POST",
         body: formData,
       });
